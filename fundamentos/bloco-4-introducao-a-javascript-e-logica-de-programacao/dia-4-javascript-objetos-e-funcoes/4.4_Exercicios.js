16a74960af1eb61f3fc1d5a1ac2d924310815e0f
Rafael Rodrigues Bizachi <rafbizachi@gmail.com>
2022-06-24 18:42:06 -0300

    resolução exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4_Exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4_Exercicios.js
@@ -3,17 +3,34 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-  console.log("Bem vinda, " + info.personagem)
+    
+};
+  //console.log("Bem vinda, " + info.personagem)
 //2
 info["recorrente"] = info.recorrente;
 info.recorrente = "Sim";
-console.log(info); 
+
+//console.log(info); 
 //3
 for(let i in info){
-console.log(i );
+//console.log(i );
 }
 //4
 for(let i in info){
-    console.log(info[i]);
+  // console.log(info[i]);
     }
+//5
+info["info2"] = info.info2;
+info.info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente : "Sim"
+}
+console.log(info.personagem + " e " + info.info2.personagem);
+console.log(info.origem + " e " + info.info2.origem);
+console.log(info.nota + " e " + info.info2.nota);
+if(info.recorrente === "Sim" && info.info2.recorrente ==="Sim"){
+console.log("Ambos são recorrentes");    
+}
+    

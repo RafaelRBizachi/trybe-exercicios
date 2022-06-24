3c82246acb8398af26e90b0924051a3819d12f52
Rafael Rodrigues Bizachi <rafbizachi@gmail.com>
2022-06-24 18:56:52 -0300

    resolução exercicios 6 7 e 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4_Exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4_Exercicios.js
@@ -33,4 +33,23 @@ console.log(info.nota + " e " + info.info2.nota);
 if(info.recorrente === "Sim" && info.info2.recorrente ==="Sim"){
 console.log("Ambos são recorrentes");    
 }
-    
+//6
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };    
+  console.log("O livro favorito de "+leitor.nome + " " +leitor.sobrenome +" se chama " + leitor.livrosFavoritos[0].titulo);
+  leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+  });
+  console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos")
